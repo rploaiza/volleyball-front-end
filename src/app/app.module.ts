29c67b3960804891b8869f81667cd4e91cafad4f
Roberth Loaiza <pauloaiza@hotmail.es>
2018-09-24 15:26:02 -0500

    Initial commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,87 @@
+import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing.module';
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { CdkTableModule } from '@angular/cdk/table';
+import { CoreModule } from './core/core.module';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { HttpModule, JsonpModule } from '@angular/http';
+import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+import { HttpService } from './core/http.service';
+import { NgModule, NO_ERRORS_SCHEMA } from '@angular/core';
+import { MDBBootstrapModule } from 'angular-bootstrap-md';
+import {
+  MatAutocompleteModule, MatButtonModule, MatButtonToggleModule,
+  MatCardModule, MatCheckboxModule, MatChipsModule,
+  MatDatepickerModule, MatDialogModule, MatExpansionModule,
+  MatGridListModule, MatIconModule, MatInputModule,
+  MatListModule, MatMenuModule, MatNativeDateModule,
+  MatPaginatorModule, MatProgressBarModule, MatProgressSpinnerModule,
+  MatRadioModule, MatRippleModule, MatSelectModule,
+  MatSidenavModule, MatSliderModule, MatSlideToggleModule,
+  MatSnackBarModule, MatSortModule, MatTableModule,
+  MatTabsModule, MatToolbarModule, MatTooltipModule,
+  MatStepperModule,
+} from '@angular/material';
+
+@NgModule({
+  imports: [
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    BrowserModule,
+    CdkTableModule,
+    CoreModule,
+    FlexLayoutModule,
+    FormsModule,
+    HttpModule,
+    MatAutocompleteModule,
+    MatButtonModule,
+    MatButtonToggleModule,
+    MatCardModule,
+    MatCheckboxModule,
+    MatChipsModule,
+    MatDatepickerModule,
+    MatDialogModule,
+    MatExpansionModule,
+    MatGridListModule,
+    MatIconModule,
+    MatInputModule,
+    MatListModule,
+    MatMenuModule,
+    MatNativeDateModule,
+    MatPaginatorModule,
+    MatProgressBarModule,
+    MatProgressSpinnerModule,
+    MatRadioModule,
+    MatRippleModule,
+    MatSelectModule,
+    MatStepperModule,
+    MatSidenavModule,
+    MatSliderModule,
+    MatSlideToggleModule,
+    MatSnackBarModule,
+    MatSortModule,
+    MatTableModule,
+    MatTabsModule,
+    MatToolbarModule,
+    MatTooltipModule,
+    ReactiveFormsModule,
+    MDBBootstrapModule.forRoot()
+  ],
+  schemas: [ NO_ERRORS_SCHEMA ],
+  declarations: [
+    AppComponent,
+    AppRoutingModule.COMPONENTS,
+    AppRoutingModule.DIALOGS_COMPONENTS,
+  ],
+  entryComponents: [AppRoutingModule.DIALOGS_COMPONENTS],
+  bootstrap: [AppComponent],
+  providers: [
+    HttpService
+  ]
+
+})
+
+export class AppModule {
+}
---
+++ src/app/home/home.component.ts
@@ -0,0 +1,29 @@
+import { Component, OnDestroy } from '@angular/core';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: 'home.component.html',
+  styleUrls: ['home.component.css']
+})
+
+export class HomeComponent implements OnDestroy {
+
+  static URL = 'home';
+
+  constructor() {
+  }
+
+  /* EJEMPLO PARA ENRUTAR
+  tickets() {
+    this.router.navigate([HomeComponent.URL, TicketsComponent.URL]);
+  }
+  */
+
+  ngOnDestroy(): void {
+    // Cerrar todas las subscripciones
+  }
+
+
+
+}
